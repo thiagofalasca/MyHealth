1416cd2ef59f67eb40f08cc97ffc501ffce12d1f
Thiago <thiagofduarte12@gmail.com>
2023-11-07 18:53:29 -0300

    jest tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { create } from 'react-test-renderer';
 import Loading from '../src/components/Loading/Loading';
 
-const loadingTest = create(<Loading />);
+const tree = create(<Loading />);
 
 test('snapshot', () => {
-  expect(loadingTest).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });
+
